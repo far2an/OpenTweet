2d26fef2fe27b8d3678eaf2b2e951bfbbbebdb8d
Farzan Abdollahi <farzan.abdollahi@zalando.de>
2024-04-20 16:24:25 +0200

    Implemented the tweet cell

---
+++ OpenTweet/Scenes/Feed/TweetCell.swift
@@ -0,0 +1,45 @@
+//
+//  TweetCell.swift
+//  OpenTweet
+//
+//  Created by Farzan Abdollahi on 20.04.24.
+//  Copyright © 2024 OpenTable, Inc. All rights reserved.
+//
+
+import UIKit
+
+struct TweetViewModel {
+    var username: String
+    var date: Date
+    var content: String
+    var imageURL: URL?
+}
+
+final class TweetCell: UICollectionViewCell, NibLoadable {
+    @IBOutlet private var imageView: UIImageView! {
+        didSet {
+            imageView.layer.cornerRadius = 20
+            imageView.layer.masksToBounds = true
+        }
+    }
+    @IBOutlet private var usernameLabel: UILabel!
+    @IBOutlet private var dateLabel: UILabel!
+    @IBOutlet private var contentLabel: UILabel!
+    
+    override func prepareForReuse() {
+        imageView.image = nil
+    }
+    
+    func update(with viewModel: TweetViewModel) {
+        imageView.setImage(with: viewModel.imageURL)
+        usernameLabel.text = viewModel.username
+        dateLabel.text = viewModel.date.string(using: .tweetDateFormatter).appendSeparator()
+        contentLabel.text = viewModel.content
+    }
+}
+
+private extension String {
+    func appendSeparator() -> String {
+        ["·", self].joined(separator: " ")
+    }
+}
